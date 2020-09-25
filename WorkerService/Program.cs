f1de51271261c6c207a0422a4698d08501278153
Micke Wessén <micke.wessen@hotmail.com>
2020-09-25 14:09:56 +0200

    Add project files.

---
+++ WorkerService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WorkerService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
